34bb00377d724ea08861fb48ca2e250a07299e33
Adrian DeRose <adrian.derose@zumer.io>
2017-05-09 20:11:57 -0700

    Initialize project

---
+++ src/Deck.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { View, Animated, Text } from 'react-native'
+
+
+class Deck extends Component {
+  render(){
+    return (
+      <View />
+    )
+  }
+}
+
+export default Deck
